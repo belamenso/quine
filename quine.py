22742c277e32dbca72f0564844ccd00dbd211597
Bartosz Białas <belamenso@gmail.com>
2019-02-12 23:53:14 +0100

    Greatly simplify

--- quine.py
+++ quine.py
@@ -1,23 +1,9 @@
 def k(s):
-    i = 0
-    while i < len(s):
-        if s[i] == '_':
-            print(s[i + 1], end='')
-            i += 2
-        else:
-            print(s[i], end='')
-            i += 1
-    print("'''" + s + "''')")
+    print(s, end='')
+    print("''" + "'" + s + "''" + "')")
 
 k('''def k(s):
-    i = 0
-    while i < len(s):
-        if s[i] == '__':
-            print(s[i + 1], end='')
-            i += 2
-        else:
-            print(s[i], end='')
-            i += 1
-    print("'_''" + s + "'_'')")
+    print(s, end='')
+    print("''" + "'" + s + "''" + "')")
 
 k(''')
